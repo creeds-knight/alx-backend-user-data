37bf630d595ff5c7c4b2d8b0d15683932b0ae230
creeds-knight <apedoarthur21@gmail.com>
2024-08-09 17:38:51 +0300

    Task-6, Basic_Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Contains a class Auth that defines the apps authentication
+"""
+from .auth import Auth
+
+class BasicAuth(Auth):
+    """
+        Performs Basic authentication
+    """
+    pass
