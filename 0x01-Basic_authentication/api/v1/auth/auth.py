d5434017d41401ae315c10b49d6b466578e0c5ba
creeds-knight <apedoarthur21@gmail.com>
2024-08-09 13:46:54 +0300

    task-4, required paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,16 @@ class Auth:
         """
             Returns False -path and excluded_paths
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        for pa in excluded_paths:
+            if pa == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> TypeVar('User'):
         """
