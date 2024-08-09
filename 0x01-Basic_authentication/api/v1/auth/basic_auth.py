9d4d02a12cc7ea80e9dcb43ba60f78b5de6c3aec
creeds-knight <apedoarthur21@gmail.com>
2024-08-10 00:40:56 +0300

    task9-user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,21 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+            Basic-User credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        try:
+            email, password = decoded_base64_authorization_header.split(":")
+        except Exception:
+            return (None, None)
+        return (email, password)
