b29e917acac3af568947cb8e281f8419907c03d3
creeds-knight <apedoarthur21@gmail.com>
2024-08-09 13:22:35 +0300

    task3, Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Contains a class Auth that defines the apps authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+        A class Auth to manage Authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+            Returns False -path and excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> TypeVar('User'):
+        """
+            Returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            Returns None
+        """
+        return None
