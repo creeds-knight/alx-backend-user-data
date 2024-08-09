ee7fc4c961b511128c4e20a8c79cf45813234a45
creeds-knight <apedoarthur21@gmail.com>
2024-08-10 00:27:30 +0300

    task8-encoding in base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
     Contains a class Auth that defines the apps authentication
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """
+            Decoding in base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
