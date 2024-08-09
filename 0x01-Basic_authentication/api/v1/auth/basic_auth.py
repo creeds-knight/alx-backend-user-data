cca50d9ff28e93b9f3de3462d96ad96772eee747
creeds-knight <apedoarthur21@gmail.com>
2024-08-10 00:13:43 +0300

    task-7, base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """
         Performs Basic authentication
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """
+            Returns the Base63 part of the Authorisation header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
