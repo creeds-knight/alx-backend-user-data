71c412da0485bc9e90155e4d4ccfa589b6b11352
creeds-knight <apedoarthur21@gmail.com>
2024-08-09 16:24:50 +0300

    task5, before_request handler

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,9 @@ class Auth:
         """
             Returns None
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
