f9685b3e61e8f4040239c1864112453a1c43a737
creeds-knight <apedoarthur21@gmail.com>
2024-08-11 16:39:42 +0300

    task1, session_auth file

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    A module to handle session Authentication
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        A class to handle session authentication
+    """
+    pass
