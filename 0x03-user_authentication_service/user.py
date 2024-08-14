179353b879660016c659294033ed8834902840ea
creeds-knight <apedoarthur21@gmail.com>
2024-08-14 20:54:52 +0300

    adding a user to database

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,8 +15,8 @@ class User(Base):
     """
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String)
+    reset_token = Column(String)
