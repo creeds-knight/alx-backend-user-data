0048bc48406b95bbc59c694d96cddec4c8f45a29
creeds-knight <apedoarthur21@gmail.com>
2024-08-18 12:32:18 +0300

    adding user to auth backend

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
     An Authentication provider(authentication backend)
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,21 @@ def _hash_password(password: str) -> bytes:
         Hashes a password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """
+        Auth class to interact with the authentication database
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+            This adds a new user to the database
+        """
+        try:
+            self._DB.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
